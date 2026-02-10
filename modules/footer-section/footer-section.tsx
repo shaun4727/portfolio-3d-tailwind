090e5066312b269b0ddb7489d2c38e4c0d5d0b9a
Shaun Hossain <shaun.mononsoft@gmail.com>
2026-02-10 21:29:46 +0600

    updated with footer

--- modules/footer-section/footer-section.tsx
+++ modules/footer-section/footer-section.tsx
@@ -2,7 +2,7 @@ export const FooterSection = () => {
     return (
         <section className="bg-primary-color mt-8 md:px-gutter py-2 md:py-4">
             <h1 className="text-white font-bold md:text-xl text-center">
-                Contact me through the options provided
+                Contact Section is under development
             </h1>
         </section>
     );
